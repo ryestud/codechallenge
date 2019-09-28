2104d623e407979b524f4ca998786fc1abdca411
ryestud <ryleystuder@u.boisestate.edu>
2019-09-28 01:16:41 -0600

    Where's my money if I have no kids?

--- calculate_expenses.php
+++ calculate_expenses.php
@@ -6,9 +6,17 @@ function echo_cost($ename, &$dnum){
     $ename = htmlspecialchars($_POST["ename"]);
     $dnum = htmlspecialchars($_POST["dnum"]);
 
+    //The employee gets a discount on their deductions if their name begins with 'A'
     if((substr(strtoupper($ename),0,1) == "A") && is_numeric($dnum)){
-        $dnum = (26 * 2000) - (500 * $dnum);
-        $dnum = ($dnum * 0.9);
+        // They have dependents
+        if($dnum != 0){
+            $dnum = (26 * 2000) - ((1000 + (500 * $dnum)) * 0.9);
+            // $dnum = ($dnum * 0.9);    
+        }
+        else{ //They have no dependents
+            $dnum = (26 * 2000);
+            $dnum = ($dnum * 0.9);    
+        }
         echo "$".$dnum;
         echo "<br>10% discount applied!";
         //add this employee to the list of all
