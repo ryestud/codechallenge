391ac01097660d9ef65fa09321c967a8623ff20b
ryestud <ryleystuder@u.boisestate.edu>
2019-09-28 22:27:45 -0600

    formatting

--- calculate_expenses.php
+++ calculate_expenses.php
@@ -9,10 +9,10 @@ function echo_cost(&$ename, &$dnum){
 
 	if(isset($_POST["ename"])){
 		$ename = htmlspecialchars($_POST["ename"]);
-    $dnum = htmlspecialchars($_POST["dnum"]);
-    $ecost = "";
-    $_SESSION["ename"] = $ename;
-    $_SESSION["dnum"] = $dnum;
+        $dnum = htmlspecialchars($_POST["dnum"]);
+        $ecost = "";
+        $_SESSION["ename"] = $ename;
+        $_SESSION["dnum"] = $dnum;
     
 
     //The employee gets a discount on their deductions if their name begins with 'A'
@@ -24,11 +24,17 @@ function echo_cost(&$ename, &$dnum){
         else{ //They have no dependents
             $ecost = (26 * 2000) - (1000 * 0.9);    
         }
-        echo "$".$ecost;
+        // echo "$".$ecost;
+        $_SESSION['ecost'] = "$".$ecost;
+        // echo "$".$ecost;
+        echo $_SESSION['ecost'];
         echo "<br>10% discount applied!";
+        
         //add this employee to the list of all
-        add_to_elist($ename, $dnum, $ecost);
-			}
+        // add_to_elist($ename, $dnum, $ecost);
+    }
+
+
       //Regular Employees 
       else if((substr(strtoupper($ename),0,1) != "A") && is_numeric($dnum)){
         // They have dependents
@@ -38,16 +44,18 @@ function echo_cost(&$ename, &$dnum){
         else{ //They have no dependents
             $ecost = (26 * 2000) - 1000;    
         }
-        echo "$".$ecost;
+        $_SESSION['ecost'] = "$".$ecost;
+        // echo "$".$ecost;
+        echo $_SESSION['ecost'];
         //add this employee to the list of all
-        add_to_elist($ename, $dnum, $ecost);
+        // add_to_elist($ename, $dnum, $ecost);
     	}
 	}
 }
 
 function add_to_elist($ename,$dnum,$ecost){
-    $dao = new Dao();
-    $dao->updateDB($ename,$dnum,$ecost);
+    // $dao = new Dao();
+    // $dao->updateDB($ename,$dnum,$ecost);
 }
 
 ?>
